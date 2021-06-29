0ae391392167f83da96eae3b866583fb2e20035b
Maryam Mohseni <maryamfmohseni@gmail.com>
2021-06-29 14:24:50 -0400

    Initial Commit

---
+++ GitHubPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubPractice
+//
+//  Created by Maryam Mohseni on 6/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
